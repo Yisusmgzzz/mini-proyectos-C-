240e8dc65a1f4240fe31ed686920fe263cfe47d3
Yisusmgzzz <jesusestebamm@gmail.com>
2025-10-25 00:45:55 -0500

    segundo commit

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -2,10 +2,26 @@
 #include <limits>
 
 int main() {
-	std::cout << "hola mundo" << std::endl;
-	std::cout << "como estas papu" << std::endl;
+	char sel;
 
-	std::cout << "maximo valor de enteros: " << std::numeric_limits<int>::max() << std::endl;
+	std::cout << "=== BIENVENIDO ===" << std::endl;
+	std::cout << "¿quieres saber el valor maximo de los numeros enteros y decimales?" << std::endl;
+	std::cout << "S/N: ";
+	std::cin >> sel;
+
+	if (sel == 'S' || sel == 's') {
+		
+		std::cout << "maximo valor de enteros: " << std::numeric_limits<int>::max() << std::endl;
+		std::cout << "maximo valor de decimales: " << std::numeric_limits<float>::max() << std::endl;
+
+		return 1;
+	}else; if (sel == 'N' || sel == 'n') {
+
+		std::cout << "oh, ok, no pasa nada ";
+		return 1;
+	}else{
+		std::cout << "eso no me servira aqui... solo puedes escoger 2 opciones";
+	}
 
 	return 0;
 }
