798fc939dbd8ce044263f189ac4e77fab7a2ea39
Yisusmgzzz <jesusestebamm@gmail.com>
2025-10-26 04:32:11 -0500

    update msg

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -11,13 +11,30 @@ void intercambio(int &numero1, int &numero2) {
     numero2 = temp;
 }
 
+//esta otra funcion es para que el usuario decida si quiere multiplicar el numero o no despues de ingresarlo
+void multiplicacion(int &numero) {
+    std::string question = "quieres multiplicar el numero? S/n ";
+    char sel;
+    std::cout <<  question << std::endl;
+    std::cin >> sel;
+    if (sel == 'N' || sel == 'n') {
+        std::cout << "Ptm.. ok." << std::endl;
+    } else {
+        numero = numero*2;
+        std::cout << "resultado: " << numero << std::endl;
+    
+    }
+}
+
 //aca se ahorran muchas lineas de codigo solo por la funcion anterior
 int main() {
     int n1, n2;
+    std::cout << "ingresa un numero: " << std::endl;
     std::cin >> n1;
-    std::cout << n1;
+    multiplicacion(n1);
+    std::cout << "ingresa un numero: " << std::endl;
     std::cin >> n2;
-    std::cout << n2;
+    multiplicacion(n2);
     //hice que preguntara el numero para hacerlo mas dinamico pero simplemente pude haber puesto un numero fijo y funciona de la misma manera
     intercambio(n1, n2);
     std::cout << "primer numero = " << n1 << " segundo numero = " << n2 << std::endl;
---
+++ taller.cpp
@@ -0,0 +1,44 @@
+/*Realice una aplicación en C++ que le pregunte al usuario la edad de su hijo.
+ * Si el niño tiene entre 0 y 6 años el programa debe imprimir que el niño pertenece al grupo de la primera infancia.
+ * Si la edad está entre los 6 y los 12 años el programa debe imprimir que el niño pertenece al grupo de la segunda infancia;
+ * y en caso de que el niño tenga entre 12 y 18 años el mensaje impreso dirá que pertenece al grupo de los adolescentes.
+ * Siga las instrucciones de la guía de aprendizaje 3*/
+
+#include <iostream>
+
+
+int main() {
+    int edad, sel;
+
+    std::cout << "==== MENU ====" << std::endl; //aqui quise hacer un menu para que mi aplicacion no se vea tan aburrida y seca jeje
+    std::cout << "¡¡que opcion quieres escoger!!" << std::endl;
+    std::cout << "1. saber de que grupo es tu hijo/a" << std::endl;
+    std::cout << "2. donar a la comunidad" << std::endl;
+    std::cout << "3. ninguna de las anteriores" << std::endl;
+    std::cin >> sel;
+
+
+    //aca aplique switch que cumple la misma funcion del if, pero para mas condiciones y hace que el codigo se vea mas uniforme.
+    switch (sel) {    
+        case 1:
+            std::cout << "¿cual es la edad de tu hijo/a?: ";
+            std::cin >> edad;
+
+            if (edad <=6) { std::cout << "eres de la primera infancia" << std::endl; }
+            if (edad <=12) {std::cout << "pertenece al grupo de la segunda infancia" << std::endl;}
+            if (edad <=18) {std::cout << "pertenece al grupo de los adolescentes" << std::endl;}
+
+            break;
+
+        case 2:
+            std::cout << "MUCHAS GRACIAS POR DONAR!! se aprecia la intencion <3" << std::endl; //una pequeña opcion para "monetizar" mi aplicacion 
+
+            break;
+
+        case 3:
+            std::cout << "Adios!! :D" << std::endl; //y la despedida 
+
+            break;
+    }
+    return 0;
+}
